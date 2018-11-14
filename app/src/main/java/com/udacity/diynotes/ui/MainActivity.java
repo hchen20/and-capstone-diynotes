f3e586b376ddb8a4faea8d0af9242e4abdf89a80
Hang Chen <hchen1991@gmail.com>
2018-11-14 01:40:57 -0800

    Implemented AsyncTask to load image

--- app/src/main/java/com/udacity/diynotes/ui/MainActivity.java
+++ app/src/main/java/com/udacity/diynotes/ui/MainActivity.java
@@ -1,7 +1,13 @@
 package com.udacity.diynotes.ui;
 
+import android.app.PendingIntent;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -9,19 +15,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.squareup.picasso.Picasso;
 import com.udacity.diynotes.AppExecutors;
 import com.udacity.diynotes.R;
 import com.udacity.diynotes.R2;
 import com.udacity.diynotes.data.database.NoteEntry;
+import com.udacity.diynotes.ui.widget.BookListWidget;
 import com.udacity.diynotes.utils.InjectorUtils;
 
 import butterknife.BindView;
@@ -31,8 +41,12 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.analytics.FirebaseAnalytics.Event;
 import com.google.firebase.analytics.FirebaseAnalytics.Param;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity  {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String BOOK_NAMES = "book_names";
 
     @BindView(R2.id.book_recycler_view)
     RecyclerView mRecyclerView;
@@ -42,6 +56,14 @@ public class MainActivity extends AppCompatActivity  {
 
     @BindView(R.id.adView)
     AdView mAdView;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.icon_image)
+    ImageView mIconImage;
+
+    private String mImageUrl;
 
     private BookListAdapter mBookListAdapter;
     private MainActivityViewModel mViewModel;
@@ -65,6 +87,10 @@ public class MainActivity extends AppCompatActivity  {
                                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                                 .build();
         mAdView.loadAd(adRequest);
+
+
+        // Attempt to load App Icon
+        new IconImageQueryTask().execute(getString(R.string.icon_image_url));
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this,
                                                     LinearLayoutManager.VERTICAL,
@@ -90,6 +116,22 @@ public class MainActivity extends AppCompatActivity  {
             mBookListAdapter.swapBooks(bookEntries);
             if (mPosition == RecyclerView.NO_POSITION) mPosition = 0;
             mRecyclerView.smoothScrollToPosition(mPosition);
+
+            // Sending Book Names to the Widget
+            Intent bookNamesIntent = new Intent(this, BookListWidget.class);
+            bookNamesIntent.putStringArrayListExtra(BOOK_NAMES, (ArrayList<String>) bookEntries);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this,
+                                                0,
+                                                bookNamesIntent,
+                                                PendingIntent.FLAG_UPDATE_CURRENT);
+
+            try {
+                pendingIntent.send();
+            } catch (Exception e) {
+                Log.d(TAG, "onCreate: " + e.getMessage());
+            }
+
 
             if (bookEntries == null) {
                 Toast.makeText(this, "There is no book currently", Toast.LENGTH_LONG).show();
@@ -142,5 +184,36 @@ public class MainActivity extends AppCompatActivity  {
                 alertDialog.show();
             }
         });
+    }
+
+    public class IconImageQueryTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if (mImageUrl != null) {
+                Picasso.get()
+                        .load(mImageUrl)
+                        .into(mIconImage);
+
+            }
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            if (isOnline()) {
+                mImageUrl = strings[0];
+            } else {
+                Toast.makeText(getApplicationContext(), getString(R.string.offline), Toast.LENGTH_LONG).show();
+            }
+            return null;
+        }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 }
--- app/src/main/java/com/udacity/diynotes/ui/widget/BookListWidget.java
+++ app/src/main/java/com/udacity/diynotes/ui/widget/BookListWidget.java
@@ -2,15 +2,22 @@ package com.udacity.diynotes.ui.widget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.udacity.diynotes.R;
+
+import java.util.List;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class BookListWidget extends AppWidgetProvider {
+    private static final String TAG = BookListWidget.class.getSimpleName();
+    private static final String BOOK_NAMES = "book_names";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -40,6 +47,37 @@ public class BookListWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        final List<String> bookNames = intent.getStringArrayListExtra(BOOK_NAMES);
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.book_list_widget);
+
+        if (bookNames != null) {
+            views.setTextViewText(R.id.appwidget_text, processBookNames(bookNames));
+
+            // This time we dont have widgetId. Reaching our widget with that way.
+            ComponentName appWidget = new ComponentName(context, BookListWidget.class);
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidget, views);
+        }
+    }
+
+    private String processBookNames(List<String> bookNames) {
+        String formattedNames = "";
+
+        for (String name: bookNames) {
+            Log.d(TAG, "processBookNames: " + name);
+            formattedNames = formattedNames + name + "\n";
+        }
+
+        return formattedNames;
     }
 }
 
